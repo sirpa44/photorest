af7d5a6c3cc82bf4ba092d8eb8071276ee476a48
Antoine <sirpa.antoine@gmail.com>
2021-09-18 11:37:08 +0200

    initialisation home

---
+++ src/Controller/PhotosController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PhotosController extends AbstractController
+{
+    public function home(): Response
+    {
+        return $this->render('photos/index.html.twig', [
+            'controller_name' => 'PhotosController',
+        ]);
+    }
+}
